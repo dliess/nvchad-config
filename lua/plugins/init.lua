e30153be7abc3b0d74f5bf53903e9730ea15efd7
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2024-07-02 10:05:03 +0200

    manjaro modifications

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,19 +5,30 @@ return {
     opts = require "configs.conform",
   },
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "vue-language-server",
+        "pyright",
+        "rust-analyzer",
+        "stylua",
+        "html-lsp",
+        "css-lsp" ,
+        "prettier"
+      }
+    }
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function ()
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
       -- require "plugins.configs.lspconfig"
       -- require "configs.lspconfig"
-    end
-  },
-  {
-    "ggandor/leap.nvim",
-    event = 'BufEnter',
-    config = function()
-      require('leap').add_default_mappings()
     end
   },
   {
@@ -35,18 +46,37 @@ return {
     }
   },
   {
-    "williamboman/mason.nvim",
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
     opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "clangd",
-        "clang-format",
-        "codelldb",
-        "vue-language-server",
-        "pyright",
-        "rust-analyzer"
-      }
-    }
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/disk@danita-hp/obsidian/obsidian_vaults_on_ipad/Personal",
+        },
+        {
+          name = "work",
+          path = "~/disk@danita-hp/obsidian/obsidian_vaults_on_ipad/Work",
+        },
+      },
+
+    -- see below for full list of options 👇
+    },
   },
   {
     "github/copilot.vim",
