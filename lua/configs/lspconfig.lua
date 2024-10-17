2b214ded128d052e21870638abc02f61d4ca163b
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2024-10-17 07:16:31 +0000

    update loading of typescript

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -17,17 +17,10 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
 }
 
 lspconfig.clangd.setup {
