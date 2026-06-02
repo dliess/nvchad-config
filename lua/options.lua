ed4b1d76ea81e24311d68b2d2e8ea8bb914663cd
Daniel Zicsi-Liess <daniel.zicsi-liess@arm.com>
2026-06-02 08:59:36 +0200

    arm indent

--- lua/options.lua
+++ lua/options.lua
@@ -6,9 +6,10 @@ vim.opt.conceallevel = 2
 vim.opt.swapfile = false
 vim.opt.colorcolumn = "80"
 -- Set the default tab behavior in insert mode to insert 4 spaces
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.expandtab = false
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
 
 vim.wo.relativenumber = true
 vim.wo.foldmethod = "expr"
